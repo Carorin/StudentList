02cad5e5e080023173705cf0aeb279bf6f20b1ce
Carorin <carorinah@gmail.com>
2021-10-28 20:05:07 +0200

    add swagger dependesis,create Student.java Class

---
+++ src/main/java/tomaszjedrzejczyk/StudentList/object/Student.java
@@ -0,0 +1,52 @@
+package tomaszjedrzejczyk.StudentList.object;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String surname;
+    private int phoneNumber;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setId(int id) {
+        this.id=id
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
